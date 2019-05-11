9569f77d3122f6260cb6993176c01ad069e3e0de
Hubert Szymański <hubertszymanski5@gmail.com>
2019-05-11 16:13:27 +0200

    add. Error Handle

--- assets/js/userList.js
+++ assets/js/userList.js
@@ -1,5 +1,6 @@
 main();
 
+// main function to run
 function main() {
     // define variable with usersAPI URL
     var usersApiURL = "http://jsonplaceholder.typicode.com/users";
@@ -7,18 +8,22 @@ function main() {
     addClickEvents();
 }
 
+// load JSON from given URL and when done add them to the list
 function loadJSON(apiURL) {
     // get list of users from the API
     $.getJSON(apiURL)
         .done(data => {
+            // when obtained data
             appendUsers(data);
         })
         .fail((jqxhr, textStatus, error) => {
             var err = "UsersAPI " + textStatus + ": " + error;
             console.log(err);
+            appendError(textStatus, error);
         });
 }
 
+// function to add users to HTML
 function appendUsers(users) {
     users.forEach(user => {
         var userHTML = 
@@ -33,6 +38,13 @@ function appendUsers(users) {
     });
 }
 
+// function to print error to HTML
+function appendError(textStatus, error) {
+    var errorHTML = '<div class="error"><i class="fas fa-exclamation-triangle"></i>' + textStatus.toUpperCase() + ': ' + error + "</div>";
+    $("#user-list").append(errorHTML);
+}
+
+// add event listeners
 function addClickEvents() {
     $("ul").on("click", "li", function () {
         $(this).children(".user-info").slideToggle();
